1cb70038c1f6f5a0c609c13d5e37f616971bf7d0
JorgeCroquer <jacroquer.18@est.ucab.edu.ve>
2023-08-25 11:24:12 -0400

    Implementing Actor Profile Bloc logic

--- lib/screens/actor-profile/bloc/actor_profile_event.dart
+++ lib/screens/actor-profile/bloc/actor_profile_event.dart
@@ -2,3 +2,9 @@ part of 'actor_profile_bloc.dart';
 
 @immutable
 sealed class ActorProfileEvent {}
+
+final class LoadActorProfile extends ActorProfileEvent {
+  final int actorId;
+
+  LoadActorProfile(this.actorId);
+}
--- lib/screens/actor-profile/bloc/actor_profile_state.dart
+++ lib/screens/actor-profile/bloc/actor_profile_state.dart
@@ -4,3 +4,13 @@ part of 'actor_profile_bloc.dart';
 sealed class ActorProfileState {}
 
 final class ActorProfileInitial extends ActorProfileState {}
+
+final class ActorProfileLoading extends ActorProfileState {}
+
+final class ActorProfileLoaded extends ActorProfileState {
+  final ActorDto actor;
+
+  ActorProfileLoaded(this.actor);
+}
+
+final class ActorProfileError extends ActorProfileState {}
