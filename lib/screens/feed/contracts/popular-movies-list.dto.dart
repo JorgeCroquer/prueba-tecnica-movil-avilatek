f0ebd4c524900c822633a7d13376a5b9382177f0
root <jorge.croquer@gpnauticals.com>
2023-08-25 08:54:31 -0400

    DTOs for feed screen

---
+++ lib/screens/feed/contracts/popular-movies-list.dto.dart
@@ -0,0 +1,31 @@
+import 'feed-movie.dto.dart';
+
+class FeedMoviesListDto {
+  final int page;
+  final List<FeedMovieDto> results;
+  final int totalPages;
+  final int totalResults;
+
+  FeedMoviesListDto({
+    required this.page,
+    required this.results,
+    required this.totalPages,
+    required this.totalResults,
+  });
+
+  factory FeedMoviesListDto.fromJson(Map<String, dynamic> json) =>
+      FeedMoviesListDto(
+        page: json['page'],
+        results: List<FeedMovieDto>.from(
+            json['results'].map((x) => FeedMovieDto.fromJson(x))),
+        totalPages: json['total_pages'],
+        totalResults: json['total_results'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'page': page,
+        'results': List<dynamic>.from(results.map((x) => x.toJson())),
+        'total_pages': totalPages,
+        'total_results': totalResults,
+      };
+}
